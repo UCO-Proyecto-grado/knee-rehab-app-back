10082e44ee90ceadb816452e25e0ae6d6762422a
Harby Garcia <harby-96@hotmail.com>
2025-05-07 13:07:34 -0500

    agregando entidad sedes.

--- lambda_instituciones/api/v1/router.py
+++ lambda_instituciones/api/v1/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from lambda_instituciones.api.v1.endpoints.tipo_sede_router import router as tipo_sede_router
 from lambda_instituciones.api.v1.endpoints.centro_rehabilitacion_router import router as centro_rehabilitacion_router
+from lambda_instituciones.api.v1.endpoints.sede_router import router as sede_router
 
 router = APIRouter(tags=["Instituciones"])
 
 router.include_router(centro_rehabilitacion_router, prefix="/centro-rehabilitacion")
 router.include_router(tipo_sede_router, prefix="/tipo-sede")
+router.include_router(sede_router, prefix="/sede")

9755418a4d978408cedf1a62e62f6e1b5a377f07
Harby Garcia <harby-96@hotmail.com>
2025-05-06 21:31:51 -0500

    Arreglando lo que falla en lo que es el consumo de api desde AWS

--- lambda_entidades_primarias/main.py
+++ lambda_entidades_primarias/main.py
@@ -3,5 +3,5 @@ from lambda_entidades_primarias.api.v1.router import router as api_router
 from mangum import Mangum
 
 app = FastAPI(title="KneeRehab API - Entidades Primarias")
-app.include_router(api_router, prefix="/api")
+app.include_router(api_router, prefix="/api/v1")
 handler = Mangum(app)
